b9da8e63eaded979ffce3a76794efba06eb8dc59
DesiringAxis <jab7540@psu.edu>
2024-02-21 18:52:17 -0500

    modified:   problem2.cpp modified:   problem2.exe new file:   problem3.cpp

--- problem2.cpp
+++ problem2.cpp
@@ -7,5 +7,8 @@ int main () {
     flights["SCE"]["PHL"] = 160;
     flights["SCE"]["ORD"] = 640;
     flights["SCE"]["EWR"] = 220;
+    cout << "Distance between SCE and PHL: " << flights["SCE"]["PHL"] << " miles" << endl;
+    cout << "Distance between SCE and ORD: " << flights["SCE"]["ORD"] << " miles" << endl;
+    cout << "Distance between SCE and EWR: " << flights["SCE"]["EWR"] << " miles" << endl;
     return 0;
 }
