3dd4c6f5d146b54ae12c7b5541e002217c32e9b6
DesiringAxis <jab7540@psu.edu>
2024-02-23 01:04:23 -0500

    new file:   problem6.cpp

---
+++ problem6.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+class Plane;
+class Pilot {
+private:
+    std::string name;
+    Plane* myPlane;
+public:
+    Pilot(const std::string& pilotName, Plane* plane) : name(pilotName), myPlane(plane) {
+        std::cout << name << " is at the gate, ready to board the plane." << std::endl;
+        std::cout << "Memory Address: " << this << std::endl;
+    }
+    ~Pilot() {
+        std::cout << name << " is out of the plane." << std::endl;
+    }
+    std::string getName() const {
+        return name;
+    }
+};
